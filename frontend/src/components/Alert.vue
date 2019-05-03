19ce97665dbbcf5c919beab20b2e87e8d444cc84
aznm12 <aznm12@archivsoft.com>
2019-05-03 16:04:22 +0900

    vuetify adapt

---
+++ frontend/src/components/Alert.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-alert
+      v-model='alert'
+      dismissible
+      type='success'
+      icon='done'
+    >This is a success alert that is closable.</v-alert>
+
+    <div class='text-xs-center'>
+      <v-btn v-if='!alert' color='primary' dark @click='alert = true'>Reset</v-btn>
+    </div>
+    <v-alert
+      :value='true'
+      color='success'
+      icon='new_releases'
+    >This is a success alert with a custom icon.</v-alert>
+
+    <v-alert :value='true' color='error'>This is an error alert with no icon.</v-alert>
+    <div class='text-xs-center'>
+      <v-btn color='primary' @click='alert = !alert'>Toggle</v-btn>
+    </div>
+    <v-alert :value='alert' type='success' transition='slide-y-transition'>This is a success alert.</v-alert>
+    <v-alert :value='true' color='success' outline>This is a success alert.</v-alert>
+
+    <v-alert :value='true' color='info' icon='info' outline>This is an info alert.</v-alert>
+
+    <v-alert :value='true' color='warning' icon='priority_high' outline>This is a warning alert.</v-alert>
+
+    <v-alert :value='true' color='error' icon='warning' outline>This is a error alert.</v-alert>
+  </div>
+</template>
+<script>
+export default {// eslint-disable-next-line
+  data() {
+    return {
+      alert: true
+    }
+  }
+}
+</script>
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-layout align-center justify-center>
+    <v-flex xs12 sm8 md4>
+      <v-form>
+        <v-text-field v-model='id' :counter='10' label='ID'></v-text-field>
+        <v-text-field v-model='password' label='Password'></v-text-field>
+        <v-card-actions>
+          <v-btn type='submit' @click='submit' color='success'>submit</v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    id: '',
+    password: ''
+  }),
+  methods: {// eslint-disable-next-line
+    submit() {
+      const headers = new Headers({
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded'
+      })
+      // fetch('http://192.168.137.59:8080/Alpha/api/auth/signin', {
+      fetch('http://localhost:8081/api/auth/signin', {
+        method: 'POST',
+        headers: headers,
+        // mode: 'cors',
+        // body: 'username=' + this.id + '&password=' + this.password
+        body: JSON.stringify({ username: this.id, password: this.pw })
+      })
+        .then(response => {
+          this.$router.push('/Alert')
+          // eslint-disable-next-line
+          return console.log('fetch response  : ' + response)
+        })
+        .catch(error => {
+          // eslint-disable-next-line
+          console.log('fetch error : ' + error)
+        })
+    }
+  }
+}
+</script>
